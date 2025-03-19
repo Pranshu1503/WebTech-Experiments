cbddeec0ddcd92c3d9ea4d96690b7efd60faa1aa
Pranshu Sharma <sharmapranshu15@gmail.com>
2025-03-19 10:47:57 +0530

    Added exp-7

---
+++ Exp-7/my-app/src/components/MainContent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MainContent() {
+    return (
+        <main className="main-content">
+            MAIN CONTENT
+        </main>
+    );
+}
+
+export default MainContent;
---
+++ Exp-7/my-app/src/components/Sidebar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Sidebar() {
+    return (
+        <aside className="sidebar">
+            <ul>
+                <li>HOME</li>
+                <li>PAGE 1</li>
+                <li>PAGE 2</li>
+            </ul>
+        </aside>
+    );
+}
+
+export default Sidebar;
